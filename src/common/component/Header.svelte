44887dd7f4f7501b821d8e8c65411f3bff2b1da8
Leehuseung <gntmd4261@naver.com>
2022-05-23 20:36:08 +0900

    헤더 메뉴 이동시 Router의 Link태그 이용방식에서 use:link를 사용하도록 변경

--- src/common/component/Header.svelte
+++ src/common/component/Header.svelte
@@ -1,56 +1,31 @@
 <script>
-    import { Router, Link, Route } from "svelte-navigator";
-    function moveSignIn(){
+    import { Router, Link, Route, link } from "svelte-navigator";
 
-    }
-
-    function moveSignUp(){
-
-    }
 </script>
 
 <nav class="navbar navbar-light">
     <div class="container">
-        <Router>
-            <a class="navbar-brand" href="index.html">conduit</a>
-            <ul class="nav navbar-nav pull-xs-right">
-                <nav>
-                    <li class="nav-item">
-                        <!-- Add "active" class when you're on that page" -->
-                        <Link to="/" >
-                            <a class="nav-link active">Home</a>
-                        </Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/editor">
-                            <a class="nav-link">
-                                <i class="ion-compose"></i>&nbsp;New Article
-                            </a>
-                        </Link>
-                    </li>
-                    <li class="nav-item">
-                        <Link to="/settings">
-                            <a class="nav-link">
-                                <i class="ion-gear-a"></i>&nbsp;Settings
-                            </a>
-                        </Link>
-                    </li>
-
-                    <li class="nav-item" >
-                        <Link to="/login" >
-                            <a class="nav-link" href="">Sign in</a>
-                        </Link>
-                    </li>
-
-
-                    <li class="nav-item">
-                        <Link to="/register">
-                            <a class="nav-link" href="">Sign up</a>
-                        </Link>
-                    </li>
-
-                </nav>
-            </ul>
-        </Router>
+        <a class="navbar-brand" href="index.html">conduit</a>
+        <ul class="nav navbar-nav pull-xs-right">
+            <li class="nav-item">
+                <a href="/" use:link class="nav-link active">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="/editor" use:link class="nav-link">
+                    <i class="ion-compose"></i>&nbsp;New Article
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="/settings" use:link class="nav-link">
+                    <i class="ion-gear-a"></i>&nbsp;Settings
+                </a>
+            </li>
+            <li class="nav-item" >
+                <a href="/login" use:link class="nav-link">Sign in</a>
+            </li>
+            <li class="nav-item">
+                <a href="/register" use:link class="nav-link">Sign up</a>
+            </li>
+        </ul>
     </div>
 </nav>
