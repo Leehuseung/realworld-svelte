2c7f0199b6b08f28023c2f719f3a8d05ad9ca0ec
Leehuseung <gntmd4261@naver.com>
2022-05-23 19:39:28 +0900

    Header,Footer,Home,Router,Register page 구현

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,29 @@
 <script>
-	export let name;
+    import { Router, Link, Route } from "svelte-navigator";
+    import Header from "./common/component/Header.svelte";
+    import Footer from "./common/component/Footer.svelte";
+    import Home from "./common/component/Home.svelte";
+    import PageNotFound from "./common/component/PageNotFound.svelte";
+    import Register from "./common/component/Register.svelte";
 </script>
 
-<main>
+<Header />
 
-</main>
+<Router>
+    <div>
+        <Route path="/">
+            <Home />
+        </Route>
+        <Route path="/register" >
+            <Register />
+        </Route>
+
+        <Route component={PageNotFound} />
+    </div>
+</Router>
+
+<Footer />
+
 
 <style>
-
 </style>
