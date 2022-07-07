d183b3251b346e8af4428a84e498e5653557bbe1
Leehuseung <gntmd4261@naver.com>
2022-07-07 13:14:58 +0900

    Sign up 구현
    - email validate

--- src/common/component/Register.svelte
+++ src/common/component/Register.svelte
@@ -1,6 +1,7 @@
 <script>
     import { link,useNavigate  } from "svelte-navigator";
     import {currentMenu, user} from "../js/store";
+    import axios from "axios";
 
     const navigate = useNavigate();
     $currentMenu = 'register';
@@ -9,13 +10,29 @@
     let email = '';
     let password = '';
     let errorHtml = '';
+    let emailValidateStyle = '';
 
     let register = function (){
         if(loginValidation()){
-            $user = { email, password };
-            currentMenu.update(() => 'home');
-            navigate("/", {
-                replace: true,
+            // $user = { email, password };
+
+            let body = {
+                'user' : {
+                    'username' : username,
+                    'email' : email,
+                    'password' : password
+                }
+            }
+
+            axios.post('/api/users',JSON.stringify(body))
+            .then(response => {
+                currentMenu.update(() => 'home');
+                navigate("/", {
+                    replace: true,
+                });
+            }).catch(error => {
+                let data = error.response.data.errors;
+                errorHtml =  '<li>'+ Object.keys(data)[0] + ' ' + data[Object.keys(data)[0]] + '</li>';
             });
         }
     }
@@ -30,12 +47,26 @@
         } else if(password === '') {
             errorHtml = '<li>password can\'t be blank</li>';
             return false;
+        } else if(!validateEmail(email)){
+            errorHtml = '<li>email is invalid</li>';
+            return false;
         }
-        // 비밀번호 맞지 않을경우
-        // else if(){
-        //     validateHtml = '<li>email or password is invalid</li>';
-        // }
+
         return true;
+    }
+
+    let validateEmail = function(email) {
+        return email == '' || String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    }
+
+    $: if(validateEmail(email)){
+        emailValidateStyle = null;
+    } else {
+        emailValidateStyle = 'red';
     }
 
 
@@ -60,10 +91,10 @@
                         <input bind:value={username} class="form-control form-control-lg" type="text" placeholder="Your Name">
                     </fieldset>
                     <fieldset class="form-group">
-                        <input  bind:value={email} class="form-control form-control-lg" type="text" placeholder="Email">
+                        <input  bind:value={email} class="form-control form-control-lg" type="text" placeholder="Email" style:border-color="{emailValidateStyle}">
                     </fieldset>
                     <fieldset class="form-group">
-                        <input class="form-control form-control-lg" type="password" placeholder="Password">
+                        <input bind:value={password} class="form-control form-control-lg" type="password" placeholder="Password">
                     </fieldset>
                     <button on:click={register} type="button"  class="btn btn-lg btn-primary pull-xs-right">
                         Sign up
