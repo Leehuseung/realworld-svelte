33217845ca026f8d9eb9954922fb1becefcf4968
Leehuseung <gntmd4261@naver.com>
2022-07-13 11:24:29 +0900

    유저정보 변경 Settings 구현

--- src/setting/component/Settings.svelte
+++ src/setting/component/Settings.svelte
@@ -1,7 +1,63 @@
 <script>
-    import {currentMenu} from "../../common/js/store";
+    import { currentMenu } from "../../common/js/store";
+    import { useNavigate } from "svelte-navigator";
+    import { user } from "../../common/js/store";
+    import axios from "axios";
+    let token = window.localStorage.getItem("jwtToken");
+    const navigate = useNavigate();
 
     $currentMenu = 'settings';
+
+    let bodyUser = {
+        'username' : null,
+        'email' : null,
+        'bio' : null,
+        'image' : null,
+        'password' : null
+    };
+
+    axios.get('/api/user', {
+        headers: {
+            Authorization: 'Token ' + token
+        }
+    }).then(res => {
+        bodyUser.username = res.data.user.username;
+        bodyUser.email = res.data.user.email;
+        bodyUser.bio = res.data.user.bio;
+        bodyUser.image = res.data.user.image;
+    }).catch(err => {
+        console.log(err);
+    });
+
+    let updateSetting = () => {
+        console.log('user',user);
+        axios.put('/api/user',JSON.stringify({
+            'user': bodyUser
+        }), {
+            headers: {
+                Authorization: 'Token ' + token
+            }
+        }).then(res => {
+            bodyUser = res.data.user;
+            $user.username = bodyUser.username;
+
+            window.localStorage.setItem("jwtToken",res.data.user.token);
+
+            navigate("/", {
+                replace: true,
+            });
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+    let logout = () => {
+        window.localStorage.removeItem("jwtToken");
+        $user = null;
+        navigate("/", {
+            replace: true,
+        });
+    }
 </script>
 
 <div class="settings-page">
@@ -10,30 +66,29 @@
 
             <div class="col-md-6 offset-md-3 col-xs-12">
                 <h1 class="text-xs-center">Your Settings</h1>
-
-                <form>
-                    <fieldset>
-                        <fieldset class="form-group">
-                            <input class="form-control" type="text" placeholder="URL of profile picture">
-                        </fieldset>
-                        <fieldset class="form-group">
-                            <input class="form-control form-control-lg" type="text" placeholder="Your Name">
-                        </fieldset>
-                        <fieldset class="form-group">
-                            <textarea class="form-control form-control-lg" rows="8"
-                                      placeholder="Short bio about you"></textarea>
-                        </fieldset>
-                        <fieldset class="form-group">
-                            <input class="form-control form-control-lg" type="text" placeholder="Email">
-                        </fieldset>
-                        <fieldset class="form-group">
-                            <input class="form-control form-control-lg" type="password" placeholder="Password">
-                        </fieldset>
-                        <button class="btn btn-lg btn-primary pull-xs-right">
-                            Update Settings
-                        </button>
+                <fieldset>
+                    <fieldset class="form-group">
+                        <input bind:value={bodyUser.image}  class="form-control" type="text" placeholder="URL of profile picture">
                     </fieldset>
-                </form>
+                    <fieldset class="form-group">
+                        <input bind:value={bodyUser.username} class="form-control form-control-lg" type="text" placeholder="Your Name">
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <textarea bind:value={bodyUser.bio} class="form-control form-control-lg" rows="8"
+                                  placeholder="Short bio about you"></textarea>
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <input bind:value={bodyUser.email} class="form-control form-control-lg" type="text" placeholder="Email">
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <input bind:value={bodyUser.password} class="form-control form-control-lg" type="password" placeholder="Password">
+                    </fieldset>
+                    <button on:click={updateSetting} class="btn btn-lg btn-primary pull-xs-right">
+                        Update Settings
+                    </button>
+                </fieldset>
+                <hr>
+                <button on:click={logout} class="btn btn-outline-danger">Or click here to logout</button>
             </div>
 
         </div>
