f1105eadac639ec291ce9b210f46f8b2a9dada82
Leehuseung <gntmd4261@naver.com>
2022-07-15 10:01:13 +0900

    Profile 컴포넌트 개선
    - ProfileDetail 생성
    - token 제거 후 store의 user객체 이용

--- src/common/component/Profile.svelte
+++ src/common/component/Profile.svelte
@@ -2,17 +2,16 @@
     import {currentMenu, user} from "../js/store";
     import { useParams } from "svelte-navigator";
     import { useNavigate } from "svelte-navigator";
+    import {noImage} from "../js/store";
     import axios from "axios";
+    import ProfileDetail from "./ProfileDetail.svelte";
     const navigate = useNavigate();
 
     $currentMenu = '@profile';
 
-    let buttonHtml = '';
-
     const params = useParams();
-
     let username = $params.username;
-    let token = window.localStorage.getItem("jwtToken");
+
     let profile = {
         'username' : null,
         'image' : null,
@@ -21,86 +20,29 @@
     }
 
     let myPage = false;
+    user.subscribe(user => {
+        if(user != null && user.username == username){
+            myPage = true;
+        }
+    });
 
     axios.get('/api/profiles/' + username).then(res => {
         profile = res.data.profile;
-        if(token != null){
-            axios.get('/api/user').then(res => {
-                if(profile.username === res.data.user.username){
-                    myPage = true;
-                }
-            });
-        } else {
-            buttonHtml = '<i class="ion-plus-round"></i> Follow ' + profile.username;
-        }
         if(profile.image === null || profile.image === ""){
-            profile.image = 'https://api.realworld.io/images/smiley-cyrus.jpeg';
+            profile.image = $noImage;
         }
     }).catch(error => {
         navigate("/", {
             replace: true,
         });
-
     });
 
-    let followEvent = () => {
-        if(myPage){
-            navigate("/settings", {
-                replace: true,
-            });
-        } else {
-            if(token == null){
-                navigate("/register", {
-                    replace: true,
-                });
-                return;
-            }
-            if(profile.following){
-                //UnFollow
-                axios.delete(`/api/profiles/${profile.username}/follow`).then(res => {
-                    profile.following = res.data.profile.following;
-                });
-            } else {
-                //Follow
-                axios.post(`/api/profiles/${profile.username}/follow`).then(res => {
-                    profile.following = res.data.profile.following;
-                });
-            }
-        }
-    }
-
-    $: if(myPage) {
-        buttonHtml = '<i class="ion-gear-a"></i> Edit Profile Settings';
-    } else {
-        if(profile.following){
-            buttonHtml = '<i class="ion-plus-round"></i> Unfollow ' + profile.username;
-        } else {
-            buttonHtml = '<i class="ion-plus-round"></i> Follow ' + profile.username;
-        }
-    }
 
 </script>
 
 <div class="profile-page">
 
-    <div class="user-info">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-xs-12 col-md-10 offset-md-1">
-                    <img src={profile.image} alt="" class="user-img"/>
-                    <h4>{profile.username}</h4>
-                    <p>
-                        {profile.bio == null ? '' : profile.bio}
-                    </p>
-                    <button on:click={followEvent} class="btn btn-sm btn-outline-secondary action-btn">
-                        {@html buttonHtml}
-                    </button>
-                </div>
-
-            </div>
-        </div>
-    </div>
+    <ProfileDetail profile="{profile}" myPage="{myPage}"/>
 
     <div class="container">
         <div class="row">
---
+++ src/common/component/ProfileDetail.svelte
@@ -0,0 +1,69 @@
+<script>
+    import {user} from "../js/store";
+    import axios from "axios";
+    import {useNavigate, useParams} from "svelte-navigator";
+    export let profile;
+    export let myPage;
+
+    const navigate = useNavigate();
+    const params = useParams();
+
+    let username = $params.username;
+
+    let followEvent = () => {
+        //나의 프로필이면 Settings로 이동
+        if(myPage){
+            navigate("/settings", {
+                replace: true,
+            });
+        } else {
+            //로그인 안했을 경우에는 회원가입 페이지로 이동
+            if($user == null){
+                navigate("/register", {
+                    replace: true,
+                });
+                return;
+            }
+            if(profile.following){
+                axios.delete(`/api/profiles/${profile.username}/follow`).then(res => {
+                    profile.following = res.data.profile.following;
+                });
+            } else {
+                axios.post(`/api/profiles/${profile.username}/follow`).then(res => {
+                    profile.following = res.data.profile.following;
+                });
+            }
+        }
+    }
+
+    let buttonHtml = '';
+    $: if(myPage) {
+        buttonHtml = '<i class="ion-gear-a"></i> Edit Profile Settings';
+    } else {
+        if(profile.following){
+            buttonHtml = '<i class="ion-plus-round"></i> Unfollow ' + profile.username;
+        } else {
+            buttonHtml = '<i class="ion-plus-round"></i> Follow ' + profile.username;
+        }
+    }
+</script>
+
+
+<div class="user-info">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-xs-12 col-md-10 offset-md-1">
+                <img src={profile.image} alt="" class="user-img"/>
+                <h4>{profile.username}</h4>
+                <p>
+                    {profile.bio == null ? '' : profile.bio}
+                </p>
+                <button on:click={followEvent} class="btn btn-sm btn-outline-secondary action-btn">
+                    {@html buttonHtml}
+                </button>
+            </div>
+
+        </div>
+    </div>
+</div>
