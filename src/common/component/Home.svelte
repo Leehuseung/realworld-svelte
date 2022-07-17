371c0583c433b5f4ceec4436ef8eae6aeae021f1
Leehuseung <gntmd4261@naver.com>
2022-07-17 23:39:54 +0900

    Popular Tags 구현

--- src/common/component/Home.svelte
+++ src/common/component/Home.svelte
@@ -19,6 +19,18 @@
         data = axios.get('/api/articles?limit=10&offset=0').then(res => res.data);
     }
 
+    $: tagData = axios.get('/api/tags').then(res => res.data);
+
+    let selectTag = '';
+    let clickTag = tag => {
+        return e => {
+            selectActive = 'tag';
+            selectTag = tag;
+            data = axios.get(`/api/articles?tag=${tag}&limit=10&offset=0`).then(res => res.data);
+        }
+    }
+
+
 </script>
 
 <div class="home-page">
@@ -44,6 +56,11 @@
                         <li class="nav-item">
                             <a on:click={globalFeedEvent} class="nav-link {selectActive === 'global' ? 'active' : ''}" href="{null}">Global Feed</a>
                         </li>
+                        {#if selectActive === 'tag'}
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{null}"># {selectTag}</a>
+                            </li>
+                        {/if}
                     </ul>
                 </div>
 
@@ -72,14 +89,22 @@
                     <p>Popular Tags</p>
 
                     <div class="tag-list">
-                        <a href="{null}" class="tag-pill tag-default">programming</a>
-                        <a href="{null}" class="tag-pill tag-default">javascript</a>
-                        <a href="{null}" class="tag-pill tag-default">emberjs</a>
-                        <a href="{null}" class="tag-pill tag-default">angularjs</a>
-                        <a href="{null}" class="tag-pill tag-default">react</a>
-                        <a href="{null}" class="tag-pill tag-default">mean</a>
-                        <a href="{null}" class="tag-pill tag-default">node</a>
-                        <a href="{null}" class="tag-pill tag-default">rails</a>
+                        {#await tagData}
+                            Loading tags...
+                        {:then tagData}
+                            {#if tagData.tags !== 0}
+                                {#each tagData.tags as tag}
+                                    <a on:click={ clickTag(tag) } href="{null}" class="tag-pill tag-default">{ tag }</a>
+                                {/each}
+                            {:else}
+                                <div class="article-preview">
+                                    No tags are here... yet.
+                                </div>
+                            {/if}
+                        {:catch error}
+                            Tag Loding error...
+                        {/await}
+
                     </div>
                 </div>
             </div>
