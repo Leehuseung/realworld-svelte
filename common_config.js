e3b359916386e673492ee6ee81f1006a9129e1ad
Leehuseung <gntmd4261@naver.com>
2022-07-18 11:17:03 +0900

    base-url 설정 추가

--- common_config.js
+++ common_config.js
@@ -1,10 +1,5 @@
-
-
-// const common_config = {
-//   "baseURL": "http://49.247.146.76"
-// }
 const common_config = {
-  "baseURL": "http://localhost:8070"
+  "baseURL": "http://49.247.146.76"
 }
 
 export default common_config;
